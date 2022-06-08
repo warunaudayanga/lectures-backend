997ad25afd7e54159a22bcc8eab799e974546f79
Waruna Udayanga <warunaudayangalakruwan@gmail.com>
2022-06-08 17:47:45 +0530

    cors test

--- src/main.ts
+++ src/main.ts
@@ -5,19 +5,23 @@ import { NestExpressApplication } from "@nestjs/platform-express";
 import * as cookieParser from "cookie-parser";
 import { AllExceptionsFilter } from "./core/filters";
 import { LoggerService } from "./core/services";
+// import helmet from "helmet";
 
 // eslint-disable-next-line func-style,@typescript-eslint/explicit-function-return-type
 async function bootstrap() {
-    const app = await NestFactory.create<NestExpressApplication>(AppModule, { logger: new LoggerService() });
+    const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+        cors: true,
+        logger: new LoggerService(),
+    });
     const { httpAdapter } = app.get(HttpAdapterHost);
     app.useGlobalFilters(new AllExceptionsFilter(httpAdapter));
-    app.use(cookieParser());
-    // app.use(helmet());
-    app.enableCors();
-    // app.setViewEngine("");
     app.useGlobalPipes(new ValidationPipe({ transform: true }));
     app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
     app.setGlobalPrefix("api");
+    app.use(cookieParser());
+    // app.use(helmet());
+    // app.enableCors();
+    // app.setViewEngine("");
     await app.listen(process.env.PORT || 3000);
 }
 // noinspection JSIgnoredPromiseFromCall
